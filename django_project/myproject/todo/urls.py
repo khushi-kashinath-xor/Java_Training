19bf04b3c00288fe5d908452dd69777250432991
Khushi Kashinath <Khushi.Kashinath@xoriant.com>
2025-11-25 14:49:52 +0530

    todo app using django

--- django_project/myproject/todo/urls.py
+++ django_project/myproject/todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('toggle/<int:task_id>/', views.toggle_complete, name='toggle_complete'),
 ]
--- django_project/myproject/todo/views.py
+++ django_project/myproject/todo/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
-from .forms import TaskForm  # import the form
+from .forms import TaskForm
 
 def home(request):
-    tasks = Task.objects.all()  # get all tasks
-    print(tasks)
-    form = TaskForm()  # create a blank form
+    tasks = Task.objects.all()
+    form = TaskForm()
 
     if request.method == "POST":
-        form = TaskForm(request.POST)  # get data from the submitted form
-        if form.is_valid():  # check if data is valid
-            form.save()  # save to database
-            return redirect('/')  # reload the page to show new task
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
     return render(request, 'home.html', {'tasks': tasks, 'form': form})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('/')
+
+def toggle_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('/')
