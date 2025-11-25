f05ac5c4286bb23ec06181daf1cde8cb1a38a39c
khushi-kashinath-xor <Khushi.Kashinath@xoriant.com>
2025-11-25 11:21:04 +0530

    Programs to implement OOPS in Java

---
+++ interface.java
@@ -0,0 +1,27 @@
+interface Payment {
+    void pay(double amount);
+}
+
+
+class CreditCard implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card.");
+    }
+}
+
+
+class UPI implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using UPI.");
+    }
+}
+
+public class PaymentSystem {
+    public static void main(String[] args) {
+        Payment p1 = new CreditCard();
+        Payment p2 = new UPI();
+
+        p1.pay(1000);
+        p2.pay(500);
+    }
+}
